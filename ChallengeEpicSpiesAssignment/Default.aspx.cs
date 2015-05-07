b7ad423a7447b4c26621941cc06e1cb94d832dfc
MikeyUchiha <mikey@starfantasygames.com>
2015-05-06 21:57:24 -0700

    Added the logic for the requirements
    Added the logic to meet the rest of the business requirements. Created
    two TimeSpan variables to calculate the differences in the calendars.
    Established the logic if the assignment is over 21 days and if the start
    of the assignment is less than 14 days.

--- ChallengeEpicSpiesAssignment/Default.aspx.cs
+++ ChallengeEpicSpiesAssignment/Default.aspx.cs
@@ -22,9 +22,26 @@ namespace ChallengeEpicSpiesAssignment
 
         protected void assignSpyButton_Click(object sender, EventArgs e)
         {
-            
-            double budget = 50.00;
-            resultLabel.Text = String.Format("Assignment of {0} to assignment {1} is authorized. Budget total: {2:C}", spyNameTextBox.Text, assignmentNameTextBox.Text, budget);
+            resultLabel.Text = "";
+            double budget = 0;
+            TimeSpan daysFromPreviousToNew = startNewCalendar.SelectedDate.Subtract(endPreviousCalendar.SelectedDate);
+            TimeSpan daysFromStartToEnd = endNewCalendar.SelectedDate.Subtract(startNewCalendar.SelectedDate);
+
+            if (daysFromStartToEnd.Days > 21)
+            {
+                budget = (daysFromStartToEnd.Days * 500) + 1000;
+            }
+            else
+                budget = (daysFromStartToEnd.Days * 500);
+
+            if(daysFromPreviousToNew.Days < 14)
+            {
+                resultLabel.Text = "Error: Must allow at least two weeks between previous assignment and new assignment.";
+                startNewCalendar.SelectedDate = DateTime.Now.Date.AddDays(14);
+            }
+            else
+                resultLabel.Text = String.Format("Assignment of {0} to assignment {1} is authorized. Budget total: {2:C}", spyNameTextBox.Text, assignmentNameTextBox.Text, budget);
+
         }
     }
 }
